2c9b6322ada8a779e750bfff64e247511abeaac6
ashokkanap <ashokjava92@gmail.com>
2020-01-18 18:21:06 -0500

    First Commit

---
+++ src/main/java/com/coreJava/DataTypeExample.java
@@ -0,0 +1,19 @@
+package com.coreJava;
+
+public class DataTypeExample {
+   // primitive DataTyes
+    int a = 10;
+   double d=10;
+   float f = 10.00f;
+   boolean b=true;
+   char c= 'k';
+   long l= 100000;
+
+   //Derived DataTypes(Non-Primitive Type)
+
+    String s1= "Bhanu";
+    int [] array = new int[4];
+    DataTypeExample dataTypeExample;
+
+
+}
---
+++ src/main/java/com/coreJava/VariableExample.java
@@ -0,0 +1,15 @@
+package com.coreJava;
+
+public class VariableExample {
+    int data=50; //instance variable
+    static int m=50; //static variable
+
+    void method()
+    {
+        int n = 60; //local variable
+    }
+
+
+}
+
+
