d45f35bed9107b055dc447a70c34c13a5bb13a77
ashokkanap <ashokjava92@gmail.com>
2020-01-19 13:27:00 -0500

    modified the file.

--- src/main/java/com/coreJava/IfElseExample.java
+++ src/main/java/com/coreJava/IfElseExample.java
@@ -2,10 +2,19 @@ package com.coreJava;
 
 public class IfElseExample {
     public static void main(String [] args){
-        int i = 9;
-        if(i == 10){
+        int p = 10;
+        if(p == 10){
        System.out.println("I am in if");
         }
+        else{
       System.out.println("I am not in if");
+    }
+    boolean status =false;
+        if(status){
+        System.out.println("I am in if");
+        }
+        else{
+            System.out.println("I am not in if");
+        }
     }
 }
